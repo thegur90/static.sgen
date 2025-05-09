95b90720f26bb2ae0476035b58b84c2d69c10b50
thegur90 <thegur90@gmail.com>
2025-05-09 13:24:58 +0300

    daily commit

--- src/transformations.py
+++ src/transformations.py
@@ -18,3 +18,14 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img","",props={"src": text_node.url,"alt": text_node.text })
     raise Exception("How did we get here? invalid Enum value?")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes: 
+        if True: pass
+
+
+
+
+    return new_nodes
